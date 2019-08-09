e4cb0e8946bdc8d5bd383da0551a51db0480170b
Mike Trout <mike.trout@gmail.com>
2019-08-09 17:19:50 +0100

    Initial commit.

---
+++ experience-service.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const port = 80;
+const app = express();
+app.use(bodyParser.json());
+
+const experience = [
+    { id: 1, jobRole: 'Technical Evangelist', dateRange: 'Nov 2017 - Present', employer: 'Amazon Web Services', description: 'I did some stuff. It was good.' },
+    { id: 2, jobRole: 'Founder, Open Source and Cloud Architect', dateRange: '2011 - 2017', employer: 'Ngineered', description: 'I did some cloud stuff. It was cloudy.' }
+];
+
+app.get('/experience', (req, resp) => {
+    console.log('Returning experience list.');
+    resp.send(experience);
+});
+
+console.log(`Experience service listening on port ${port}`);
+app.listen(port);
