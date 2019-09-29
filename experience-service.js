018c00b8776268a0f80690a2d206831d688b2edf
Mike Trout <mike.trout@gmail.com>
2019-09-29 17:25:05 +0100

    Remove body-parser dependency.

--- experience-service.js
+++ experience-service.js
@@ -1,10 +1,7 @@
 const express = require('express');
-// const path = require('path');
-const bodyParser = require('body-parser');
 
 const port = 50000;
 const app = express();
-app.use(bodyParser.json());
 
 const experience = [
     { id: 1, jobRole: 'Analyst Programmer', dateRange: 'Jan 2011 - Present', employer: 'Covea Insurance plc.', description: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam adipiscing vitae proin sagittis nisl. Arcu felis bibendum ut tristique et egestas quis. Pharetra diam sit amet nisl suscipit adipiscing bibendum est ultricies. Eu volutpat odio facilisis mauris sit. Volutpat odio facilisis mauris sit. Arcu vitae elementum curabitur vitae nunc sed. Et egestas quis ipsum suspendisse ultrices gravida dictum. Sed turpis tincidunt id aliquet risus feugiat. Fermentum leo vel orci porta non. Blandit cursus risus at ultrices. Vitae purus faucibus ornare suspendisse sed nisi lacus. Scelerisque varius morbi enim nunc faucibus a. Neque aliquam vestibulum morbi blandit cursus risus at ultrices mi. Commodo sed egestas egestas fringilla. Feugiat sed lectus vestibulum mattis ullamcorper. Imperdiet proin fermentum leo vel orci.</p><p>Laoreet sit amet cursus sit amet. Et ultrices neque ornare aenean euismod elementum. Tristique senectus et netus et malesuada fames ac. Sagittis aliquam malesuada bibendum arcu vitae. Volutpat est velit egestas dui id ornare arcu odio. Enim facilisis gravida neque convallis a. Sit amet mauris commodo quis imperdiet massa. Tortor dignissim convallis aenean et tortor. Viverra ipsum nunc aliquet bibendum enim facilisis. Lacinia quis vel eros donec ac odio tempor. Libero id faucibus nisl tincidunt eget. Nec feugiat in fermentum posuere urna nec tincidunt praesent.</p>' },
@@ -12,12 +9,11 @@ const experience = [
 ];
 
 app.get('/api/experience', (req, resp) => {
-    // console.log('Returning experience list.');
     resp.send(experience);
 });
 
-console.log(`Experience service listening on port ${port}`);
 const server = app.listen(port);
+console.log(`Experience service listening on port ${port}`);
 
 // The signals to handle
 // NOTE: The SIGKILL signal (9) cannot be intercepted and handled
